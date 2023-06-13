a05d69f4899dbb0ad9b0672fccecf96d7be34b53
Ingrid Benicio <78183082+ingrid-benicio@users.noreply.github.com>
2023-06-13 20:40:13 -0300

    list comprehension

--- teste.py
+++ teste.py
@@ -14,6 +14,36 @@
 #         print (i)
 
 
+# using list comprehension
+
+# numeros = list(range(1,101))
+
+# num_pares = [numero for numero in numeros if numero % 2 == 0 and numero % 4 == 0]
+# #esse primeiro numero é o retorno da coisa toda, é o que vai "receber" o resultado do que vier depois
+# print(num_pares)
+
+numeros = list(range(1,101))
+
+numeros_impares = [ numero for numero in numeros if numero % 2== 1 and numero % 4 == 1]
+
+print(numeros_impares)
+print(type(numeros_impares),type(numeros))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
